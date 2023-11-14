fc413850a1f84f62543320ff430fd085bd475dc7
05809966543 <05809966543@residencia-656>
2023-11-14 15:05:10 -0300

    Acrescentando métodos e atributos na classe criada em sala. Aula 02.

--- Aula-02/src/Conta.java
+++ Aula-02/src/Conta.java
@@ -5,15 +5,21 @@ public class Conta {
 	private float saldo;
 	
 	public void inicializa(String n, float s) {
-		
+		nome = n;
+		saldo = s;
+		if(saldo < 0) {
+			System.out.println("Não pode criar uma conta com saldo devedor.");
+		}
 	}
 	
 	public void deposita(float valor) {
-		
+		saldo = saldo + valor;
 	}
 	
 	public void consulta() {
-		
+		System.out.println("Cliente: " + nome);
+		System.out.println("Número da conta: " + numero);
+		System.out.println("Saldo atual: " + saldo);
 	}
 	
 	public int saque(float valor) {
