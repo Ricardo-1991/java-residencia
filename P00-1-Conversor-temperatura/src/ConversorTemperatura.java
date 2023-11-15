535d4ed8295b22a31c4ea497d999e055420fda4e
Ricardo <ricardo.dev1991@gmail.com>
2023-11-14 22:25:00 -0300

    Realizada atividade de conversor de temperaturada da P00-1

---
+++ P00-1-Conversor-temperatura/src/ConversorTemperatura.java
@@ -0,0 +1,16 @@
+import java.text.DecimalFormat;
+
+public class ConversorTemperatura {
+	
+	public static float converte(float temperature, int option) {
+		float result = 0;
+		DecimalFormat format = new DecimalFormat("#.#");
+		if(option == 1) {
+			result = (temperature * 9/5) + 32;
+		}else if(option == 2) {
+			result = ((temperature - 32) * 5) / 9;
+		} 
+		
+		return result;
+	}
+}
